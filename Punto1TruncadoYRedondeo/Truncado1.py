c3dbcdddd39110587b7ad4ad33161307d7acb1bc
Jose Gonzalo Scali <94322853+GonziFlowReloaded@users.noreply.github.com>
2023-04-10 20:41:53 -0300

    drive

---
+++ Punto1TruncadoYRedondeo/Truncado1.py
@@ -0,0 +1,26 @@
+import math
+
+def truncate(number: float, max_decimals: int) -> float:
+    int_part, dec_part = str(number).split(".")
+    return float(".".join((int_part, dec_part[:max_decimals])))
+
+
+var1 = 13/14
+
+var2 = 6/7
+
+var3 = 2 * math.e
+
+truncado1 = truncate(float(var1), 3)
+truncado2 = truncate(float(var2), 3)
+truncado3 = truncate(float(var3), 3)
+
+
+print('13/14 Truncado: {}'.format(truncado1))
+print('6/7 Truncado: {}'.format(truncado2))
+print('2e Truncado: {}'.format(truncado3))
+
+resta = truncate(truncado1 - truncado2, 3)
+division = truncate(resta/truncado3, 3)
+print('13/14 - 6/7 Truncado: {}'.format(resta))
+print('(13/14 - 6/7)/2e Truncado: {}'.format(division))
