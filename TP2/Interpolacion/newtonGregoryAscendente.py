852d7028937df9c818a036478b8dd53ef2576b43
Thiago <thiagofalkiev@hotmail.com.ar>
2023-05-19 14:13:55 -0300

    Agregar Documentación
    Se documentó y se ordenó de una mejor manera el código.

--- TP2/Interpolacion/newtonGregoryAscendente.py
+++ TP2/Interpolacion/newtonGregoryAscendente.py
@@ -1,21 +1,50 @@
 import numpy as np
 
 def error_porcentual(valor_aproximado, valor_real):
+
+    """
+    Calcula el error porcentual entre un valor aproximado y un valor real.
+
+    Args:
+        valor_aproximado (float): Valor aproximado.
+        valor_real (float): Valor real.
+
+    Returns:
+        float: Error porcentual.
+    """
+
     return abs((valor_real - valor_aproximado) / valor_real) * 100
     
 
 def gregory_newton(x, y, target):
+
+    """
+    Implementa el método de interpolación de Newton-Gregory ascendente.
+
+    Args:
+        x (list): Lista de puntos x conocidos.
+        y (list): Lista de puntos y conocidos.
+        target (float): Punto en el que se desea interpolar.
+
+    Returns:
+        float: Valor interpolado en el punto objetivo.
+    """
+
     n = len(x)
     tabla = [[0 for j in range(n)] for i in range(n)]
 
+    # Inicializar primera columna de la tabla con los valores de y
     for i in range(n):
         tabla[i][0] = y[i]
 
+    # Calcular los coeficientes interpolantes restantes en la tabla
     for j in range(1, n):
         for i in range(n - j):
             tabla[i][j] = (tabla[i + 1][j - 1] - tabla[i][j - 1]) / (x[i + j] - x[i])
 
     resultado = tabla[0][0]
+
+    # Calcular el resultado interpolado
     for j in range(1, n):
         producto = 1
         for i in range(j):
@@ -24,5 +53,3 @@ def gregory_newton(x, y, target):
 
 
     return resultado
-
-
--- TP2/IterativosLineales/jacobi.py
+++ TP2/IterativosLineales/jacobi.py
@@ -1,18 +1,38 @@
 import numpy as np
 
 def jacobi(A, b, x0, tol=1e-6, max_iterations=100):
+
+    """
+    Implementación del método de Jacobi para resolver un sistema de ecuaciones lineales.
+
+    Args:
+        A: Matriz de coeficientes del sistema.
+        b: Vector de términos independientes.
+        x0: Vector inicial de soluciones.
+        tol: Tolerancia para la convergencia.
+        max_iterations: Número máximo de iteraciones.
+
+    Returns:
+        x_new: Vector de soluciones aproximadas.
+
+    """
+
     n = len(A)
     x = x0.copy()
     x_new = np.zeros_like(x)
 
     for k in range(max_iterations):
+        # Iterar sobre cada fila de la matriz
         for i in range(n):
             sum_term = 0
+             # Calcular la suma de los términos de la ecuación
             for j in range(n):
                 if j != i:
                     sum_term += A[i][j] * x[j]
+            # Calcular la nueva aproximación de x[i]
             x_new[i] = (b[i] - sum_term) / A[i][i]
-        
+            
+        # Verificar la convergencia
         if np.linalg.norm(x_new - x) < tol:
             break
         
