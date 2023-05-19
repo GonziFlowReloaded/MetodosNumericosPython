97ce9534b32b427af40faf3b99064e282ffb35bf
Jose Gonzalo Scali <gonzalojoses@hotmail.com>
2023-05-19 11:01:08 -0300

    fix

--- TP2/IterativosLineales/gaussSiedel.py
+++ TP2/IterativosLineales/gaussSiedel.py
@@ -1,34 +1,24 @@
 import numpy as np
 
-def gauss_seidel(A, b, x_init, tol=1e-6, max_iter=100):
-  """
-  Solves the linear system Ax = b using the Gauss-Seidel method.
+def gauss_seidel(A, b, x0, tol, max_iter):
+    n = len(A)
+    x = x0.copy()
+    iter_count = 0
+    error = tol + 1
 
-  Args:
-    A: The coefficient matrix.
-    b: The right-hand side vector.
-    x_init: The initial guess for the solution.
-    tol: The tolerance for convergence.
-    max_iter: The maximum number of iterations.
+    while error > tol and iter_count < max_iter:
+        x_new = x.copy()
+        for i in range(n):
+            s1 = sum(A[i][j] * x_new[j] for j in range(i))
+            s2 = sum(A[i][j] * x[j] for j in range(i + 1, n))
+            x_new[i] = (b[i] - s1 - s2) / A[i][i]
+        
+        error = max(abs(x_new[i] - x[i]) for i in range(n))
+        x = x_new
+        iter_count += 1
 
-  Returns:
-    The solution to the linear system.
-  """
-
-  # Initialize the solution.
-  x = x_init
-
-  # Iterate until convergence or the maximum number of iterations is reached.
-  for i in range(max_iter):
-
-    # Update the solution.
-    for j in range(A.shape[0]):
-      x[j] = (b[j] - A[j, :].dot(x)) / A[j, j]
-
-    # Check for convergence.
-    if np.linalg.norm(x - x_init) < tol:
-      break
-
-  return x
-
+    if iter_count == max_iter and error > tol:
+        print("Gauss-Seidel method did not converge within the specified tolerance.")
+    
+    return x
 
